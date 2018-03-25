f688a41d70b9b8602aa6385e7822b6855ca493c4
fridab <frida.bornfors@gmail.com>
2018-03-25 19:54:51 +0200

    Check out files

--- mainFile.py
+++ mainFile.py
@@ -1,1 +1,13 @@
-print("Second commit")
+# Import statements
+import numpy as np
+import pandas as pd
+
+# Read data
+train_df = pd.read_csv('..\\training_variants')
+text_df = pd.read_csv('..\\training_text', sep = '\|\|', header = None, skiprows = 1, names = ['ID', 'Text'], engine = 'python', encoding = 'utf-8')
+
+#Check out the data frames
+print(train_df.shape)
+print(train_df.head(5))
+print(text_df.shape)
+print(text_df.head(5))
